3f8b40421649e0a3d7742ac393feba31b5099e9e
Everton Cavalcante <evertonranielly@gmail.com>
2025-09-06 18:50:52 -0300

    Pushing Javadoc to GitHub

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Benchmark"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"ParentProcess"},{"p":"<Unnamed>","l":"Task"}];updateSearchResults();
